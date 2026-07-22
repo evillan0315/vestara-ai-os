82dcf8d239ca91e876a18bcf219a3a98511c270c
Eddie Villanueva <evillan0315@gmail.com>
2026-07-22 18:04:14 +0800

    fix: Enable vertical scrollbar in dashboard pages when content exceeds screen height

--- apps/dashboard/src/components/DashboardLayout.tsx
+++ apps/dashboard/src/components/DashboardLayout.tsx
@@ -194,7 +194,7 @@ export function DashboardLayout() {
           )}
 
           {/* Main content */}
-          <main className="flex-1 min-h-0 overflow-hidden flex flex-col">
+          <main className="flex-1 min-h-0 overflow-auto flex flex-col">
             <ErrorBoundary key={location.pathname}>
               <div className="flex-1 flex flex-col overflow-hidden widget-enter">
                 <Outlet />
